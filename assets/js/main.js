69f812780ad21e749d95d28f29170b94513a8142
mcquade3 <19432096+mcquade3@users.noreply.github.com>
2020-01-01 23:00:41 -0500

    Add content to site

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,38 @@
 		$('.scrolly').scrolly();
 
 })(jQuery);
+
+
+
+/* Back to Top Button */
+const scrollToTopButton = document.getElementById('js-top');
+
+const scrollFunc = () => {
+	// Get the current scroll value
+	let y = window.scrollY;
+	 
+	// If the scroll value is greater than the window height, add a class to the scroll-to-top button to show it
+	if (y > 0) {scrollToTopButton.className = "top-link show";}
+	else {scrollToTopButton.className = "top-link hide";}
+};
+
+window.addEventListener("scroll", scrollFunc);
+
+const scrollToTop = () => {
+	// Number of pixels the user is from the top of the document
+	const c = document.documentElement.scrollTop || document.body.scrollTop;
+	 
+	// If the number is greater than 0, scroll back to 0, or the top of the document
+	if (c > 0) {
+		window.requestAnimationFrame(scrollToTop);
+		// ScrollTo takes an x and a y coordinate.
+		// Increase the '10' value to get a smoother/slower scroll
+		window.scrollTo(0, c - c / 6);
+	}
+};
+
+// When the button is clicked, run ScrolltoTop function
+scrollToTopButton.onclick = function(e) {
+	e.preventDefault();
+	scrollToTop();
+}
